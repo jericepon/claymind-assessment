96d902dc9964e738d324d30e2762ac11fb6caed2
jericepon <jericepon@gmail.com>
2021-10-30 02:15:32 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+        <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
